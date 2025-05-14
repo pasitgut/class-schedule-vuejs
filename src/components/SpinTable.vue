65a59e495fff1f06d346a14e67ce9e40ab0f771e
pasitgut <pasitgut.dev@gmail.com>
2025-05-14 16:33:52 +0700

    First commit

---
+++ src/components/SpinTable.vue
@@ -0,0 +1,26 @@
+<template>
+    <div>
+        <span class="centered half-screen z-50">
+            <span class="animate-ping absolute h-full w-full rounded-full bg-grey-500 opacity-75"></span>
+        </span>
+        <div class="loader-wrapper lmask"></div>
+    </div>
+</template>
+
+<style>
+div.masked {
+    position: relative;
+}
+
+div.masked:before {
+    position: absolute;
+    content: "";
+    width: 100%;
+    height: 100%;
+    background-color: rgb(114, 110, 110, 0.7)
+}
+
+span.ping {
+    background-color: rgb(143, 174, 144)
+}
+</style>
---
+++ tailwind.config.js
@@ -0,0 +1,36 @@
+// const gridConfig = () => {
+//   const gridColumnStart = {}
+//   const gridColumnEnd = {}
+//   const gridTemplateColumns = { 156: 'repeat(156, minmax(0, 1fr))' }
+
+//   for (let i = 14; i <= 158; i++) {
+//     gridColumnStart[i.toString()] = i.toString()
+//     gridColumnEnd[i.toString()] = i.toString()
+//   }
+
+//   return { gridTemplateColumns, gridColumnStart, gridColumnEnd }
+// }
+
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ['./public/**/*.html', './src/**/*.vue'],
+  safelist: [
+    {
+      pattern: /col-(start|end)-(.)*/,
+    },
+  ],
+  darkMode: 'selector', // or 'media' or 'class'
+  theme: {
+    extend: {
+      ...gridConfig(),
+      minHeight: (theme) => ({
+        ...theme('spacing'),
+      }),
+      minWidth: {
+        1000: '1000px',
+      },
+      hidden: {},
+    },
+  },
+  plugins: [],
+}
